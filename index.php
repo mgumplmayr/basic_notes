d7c9b660a5ffd77f576352dff4dc21c0b0167516
Koracs <115572388+Koracs@users.noreply.github.com>
2023-10-31 14:43:55 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="/global.css">
+</head>
+<body>
+    <nav>
+        <a href = "/basic/index.php">Home </a>
+        <a href = "/rest/index.php">Home (REST)</a>
+        <a href = "/soap/calculate.php">Calculator (SOAP)</a>
+        <a href = "/soap/soapserver.php?wsdl">WSDL</a>
+    </nav>
+</body>
